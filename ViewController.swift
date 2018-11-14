750ff925e1cdf3555df057eb5ead26a4aed655ef
Donkemezuo <raymonddonkemezuo.org>
2018-11-14 14:23:39 -0500

    Autolayout exercise

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autolayout again
+//
+//  Created by Donkemezuo Raymond Tariladou on 11/14/18.
+//  Copyright © 2018 EnProTech Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
